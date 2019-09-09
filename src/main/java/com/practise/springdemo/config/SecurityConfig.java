63ee5f02b6cb908276414d13d6fef9c6d3fc2704
AbiSelvakumar <abiramiselvakumarp@yahoo.com>
2019-09-09 05:57:00 -0400

    Updated Security Changes in my Demo code

--- src/main/java/com/practise/springdemo/config/SecurityConfig.java
+++ src/main/java/com/practise/springdemo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.practise.springdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,7 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	     http.csrf().disable();
+	}
 
 	@Bean
 	@Override
@@ -33,5 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 		return new InMemoryUserDetailsManager(user1,user2);
 	}
+
+
 
 }
